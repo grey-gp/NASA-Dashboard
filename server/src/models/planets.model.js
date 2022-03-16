255450b78e1624ca80af2f790257a49c76857767
grey-gp <grey.grahamp@gmail.com>
2022-03-16 12:49:58 -0400

    A new git repo for the Nasa Dashboard

---
+++ server/src/models/planets.model.js
@@ -0,0 +1,39 @@
+const { parse } = require('csv-parse');
+const fs = require('fs');
+const path = require('path');
+
+const habitablePlanets = [];
+
+const isHabitiablePlanet = (planet) => {
+    return planet['koi_disposition'] === 'CONFIRMED'
+        && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+        && planet['koi_prad'] < 1.6;
+}
+
+function loadPlanetsData() {
+    return new Promise((resolve, reject) => {
+        fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
+            .pipe(parse({
+                comment: '#',
+                columns: true,
+            }))
+            .on('data', data => {
+                if (isHabitiablePlanet(data))
+                    habitablePlanets.push(data);
+            })
+            .on('error', error => {
+                console.log(error);
+                reject(error);
+            })
+            .on('end', () => {
+                console.log(`The kepler telescope found ${habitablePlanets.length} planets that could be habitable`);
+                resolve();
+            });
+    });
+}
+
+
+module.exports = {
+    loadPlanetsData,
+    planets: habitablePlanets,
+};
---
+++ server/src/server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const app = require('./app');
+const PORT = process.env.PORT || 8000;
+
+const { loadPlanetsData } = require('./models/planets.model');
+
+const server = http.createServer(app);
+
+async function startServer() {
+    await loadPlanetsData();
+    server.listen(PORT, () => {
+        console.log(`Listening at http://localhost:${PORT}`);
+    });
+}
+
+startServer();
