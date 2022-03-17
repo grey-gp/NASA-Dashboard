2a95fd0d1fae2382b4e492345f258e63d2c78a0d
Graham Pennington <grey.grahamp@gmail.com>
2022-03-17 14:12:15 -0400

    Added launches model, router, and controller as well as fixed routing when refreshing pages

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 
 const planetsRouter  = require('./routes/planets/planets.router');
+const launchesRouter = require('./routes/launches/launches.router');
 
 const app = express();
 
@@ -13,5 +14,9 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use(planetsRouter);
+app.use(launchesRouter);
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 module.exports = app;
