6b48da783de7e151a6908275b7b55c78a6ed6bbb
grey-gp <grey.grahamp@gmail.com>
2022-03-22 10:15:02 -0400

    Began adding functionality for Mongoose, added schemas for both launches and planets.

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,26 @@
 const http = require('http');
+const mongoose = require('mongoose');
 
 const app = require('./app');
 const PORT = process.env.PORT || 8000;
+const MONGO_URL = 'mongodb+srv://nasa-api:Ul5ChF3IPSErZI2q@nasacluster.beqv4.mongodb.net/nasa?retryWrites=true&w=majority';
 
 const { loadPlanetsData } = require('./models/planets.model');
 
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+    console.log('Connection to MongoDB established...');
+});
+
+mongoose.connection.on('error', err => {
+    console.error(err);
+});
+
 async function startServer() {
+
+    await mongoose.connect(MONGO_URL);
+
     await loadPlanetsData();
     server.listen(PORT, () => {
         console.log(`Listening at http://localhost:${PORT}`);
