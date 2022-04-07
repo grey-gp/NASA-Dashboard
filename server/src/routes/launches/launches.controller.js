4ba5da1b7396fa648fe201f90581bfdebcd90661
grey-gp <grey.grahamp@gmail.com>
2022-04-06 21:50:22 -0400

    Updated the launches to use mongoDB

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,10 +1,14 @@
-const { existsWithId, getAllLaunches, addNewLaunch, abortLaunchById } = require('../../models/launches.model');
+const { 
+    existsWithId, 
+    getAllLaunches, 
+    scheduleNewLaunch, 
+    abortLaunchById } = require('../../models/launches.model');
 
-function httpGetAllLaunches(req, res) {
-    return res.status(200).json(getAllLaunches());
+async function httpGetAllLaunches(req, res) {
+    return res.status(200).json(await getAllLaunches());
 }
 
-function httpAddNewLaunch(req, res) {
+async function httpAddNewLaunch(req, res) {
     const launch = req.body;
 
     if (!launch.mission || !launch.rocket || !launch.launchDate
@@ -21,21 +25,30 @@ function httpAddNewLaunch(req, res) {
         });
     }
 
-    addNewLaunch(launch);
+    await scheduleNewLaunch(launch);
     return res.status(201).json(launch);
 }
 
-function httpAbortLaunch(req, res) {
+async function httpAbortLaunch(req, res) {
     const launchId = Number(req.params.id);
     
-    if (!existsWithId(launchId)) {
+    const launchExistsWithId = await existsWithId(launchId);
+    if (!launchExistsWithId) {
         return res.status(404).json({
             error: 'No launch found with id',
         });
     }
     
-    const aborted = abortLaunchById(launchId);
-    return res.status(200).json(aborted);
+    const aborted = await abortLaunchById(launchId);
+
+    if (!aborted) {
+        return res.status(400).json({
+            error: 'Launch not aborted',
+        });
+    }
+    return res.status(200).json({
+        ok: true,
+    });
 }
 
 module.exports = {
